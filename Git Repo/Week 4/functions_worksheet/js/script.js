9e46ac28e0f6c79cb5d7cc9e2d7c138ad624f69c
Pbailey <matthewbailey@Matthews-MacBook-Pro.local>
2015-06-19 21:54:41 -0400

    FUNCTION

--- Git Repo/Week 4/functions_worksheet/js/script.js
+++ Git Repo/Week 4/functions_worksheet/js/script.js
@@ -18,3 +18,16 @@ var ans = calcCircumference(2, 3.14, 6); //invoking
 //After machine works out the problem it will print "The circumference of your circle is X"
 console.log("The circumference of your circle is " +  ans);
 
+
+//Sting!
+
+//Calculate the amount of Bee sting it takes to kill an animal.
+var victimWeight = function(weight, sting){
+    //code the function runs
+    var amountSting = weight * sting;
+    return amountSting;
+}
+
+var answer = victimWeight(600, 8.666666667); //invoking
+//After machine works out the problem it will print "It takes X bee stings to kill this animal.
+console.log ("It takes " + answer + " bee stings to kill this animal.");
