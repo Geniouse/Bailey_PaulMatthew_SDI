c01add19e133225f019f9b636827e982ef0cab99
Pbailey <matthewbailey@Matthews-MacBook-Pro.local>
2015-06-23 19:59:46 -0400

    First variable

---
+++ Git Repo/Week 4/Bailey_Paul-Matthew_Functions_Assignment/js/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by matthewbailey on 6/23/15.
+ */
+//Paul-Matthew Bailey
+//SDI Assignment: Function
+
+//Variables
+var nameInput = prompt("Please enter your name"); //Prompt for user to enter name.
+
