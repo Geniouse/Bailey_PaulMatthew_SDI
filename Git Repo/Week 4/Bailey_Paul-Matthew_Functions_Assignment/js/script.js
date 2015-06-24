d13b110030cb7edd3278cc1673cfb735fee2eb25
Pbailey <matthewbailey@Matthews-MacBook-Pro.local>
2015-06-23 20:13:12 -0400

    Name prompt & Validation

--- Git Repo/Week 4/Bailey_Paul-Matthew_Functions_Assignment/js/script.js
+++ Git Repo/Week 4/Bailey_Paul-Matthew_Functions_Assignment/js/script.js
@@ -7,3 +7,23 @@
 //Variables
 var nameInput = prompt("Please enter your name"); //Prompt for user to enter name.
 
+//functions
+
+function nameEntered(firstName){
+
+    var timesClicked = 1;
+
+    while (firstName === ""){
+
+        firstName = prompt("You forgot to enter a name!");
+
+        timesClicked++;
+
+        if (timesClicked >= 3){
+
+            console.log("You've exceeded the maximum amount of attempts!");
+            break;
+        }
+     }
+
+    return firstName;
